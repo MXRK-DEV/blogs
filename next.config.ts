a751336126968e6ea40b41f4186f0b55a149cdf3
mxrk-dev <damark215@gmail.com>
2025-01-30 04:49:24 +0100

    .

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  distDir: 'dist', // Custom build output directory
 };
 
 export default nextConfig;
